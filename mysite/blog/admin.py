d18b1bf169b4bf3f0ba2d026f5f45e59fc72601d
ferama <ferama@npc.(none)>
2010-10-03 21:03:28 +0200

    removed mysite

--- mysite/blog/admin.py
+++
@@ -1,9 +0,0 @@
-from blog.models import Post
-from blog.models import  Category
-from django.contrib import admin
-
-class PostAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Post, PostAdmin)
-admin.site.register(Category)
