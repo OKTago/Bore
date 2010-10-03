4fda5655270248c15444c9785782ce6dcae01d3a
ferama <ferama@npc.(none)>
2010-10-03 21:00:13 +0200

    renamed base apps dir

---
+++ apps/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls.defaults import *
+import settings
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^apps/', include('apps.foo.urls')),
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^$', 'apps.blog.views.index'),
+    (r'^blog/', include('apps.blog.urls')),
+    (r'^comments/', include('django.contrib.comments.urls')),
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
