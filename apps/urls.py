bac62d05883a86aed596744bbd7ca0f14eb804b9
ferama <ferama@npc.(none)>
2010-10-17 13:08:05 +0200

    apache problems solved

--- apps/urls.py
+++ apps/urls.py
@@ -11,7 +11,8 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^face/', include('apps.face.urls')),
+    #(r'^$', home), 
+    (r'^', include('apps.face.urls')),
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 )
