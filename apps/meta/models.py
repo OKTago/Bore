6305063065488570fbdf5261def1de883b7624ef
ferama <ferama@npc.(none)>
2010-10-19 22:52:36 +0200

    updates

--- apps/meta/models.py
+++ apps/meta/models.py
@@ -6,13 +6,17 @@ from django.db.utils import DatabaseError
 from django.contrib import admin
 
 def person_unicode(self): return self.name
-Person = type('Person', (models.Model,), {
+Object = type('Person', (models.Model,), {
     '__module__': 'meta.models', # deve corrispondere al path di import per questo file 
     'name': models.CharField(max_length=255),
     '__unicode__': person_unicode,
 })
 
-admin.site.register(Person)
+admin.site.register(Object)
 
 # TODO: cerca un modo per fare il syncdb in automatico
 #       Vedi django/core/management/commands/syncdb.py
+
+# TODO: verificare se esiste una sorta di elenco di models.Fields
+#       in modo che nell'interfaccia di definizione del tipo
+#       si possa usare un drop-down con l'elenco
