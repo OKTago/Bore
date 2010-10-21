e466c0a6c0514b6486956e3c29c9454f605eadef
ferama <ferama@ferama-desktop.(none)>
2010-10-21 13:37:18 +0200

    metaMan

--- apps/meta/models.py
+++ apps/meta/models.py
@@ -7,6 +7,26 @@ from django.contrib import admin
 
 from meta.builder import *
 
+class MetaMan:
+    __metaClasses = {}
+
+    def addModel(self, cls):
+        objName = cls._meta.object_name 
+        self.__metaClasses[objName] = cls
+
+    def removeModel(self, cls):
+        pass
+
+    def getInstance(self, typeName):
+        cls = self.__metaClasses[typeName]
+        obj = cls()
+        return obj
+
+    def getClass(self, typeName):
+        return self.__metaClasses[typeName]
+
+metaMan = MetaMan()
+
 Object = type('Person', (models.Model,), {
     '__module__': 'meta.models', # deve corrispondere al path di import per questo file 
     'name': models.CharField(max_length=255),
@@ -28,7 +48,25 @@ Document = type('Document', (models.Model,), {
     '__unicode__': lambda self: eval("self."+"title"),
 })
 
+for i in range(3):
+    base = type('Type'+str(i), (models.Model,), {
+        '__module__': 'meta.models',
+        'name': models.CharField(max_length=255),
+        '__unicode__': lambda self: eval("self."+"name"),
+    })
+    metaMan.addModel(base)
+    admin.site.register(base)
+
+# eredita da Type0
+obj = type('TypeExt', (metaMan.getClass('Type0'),), {
+    '__module__': 'meta.models',
+    'nameExt': models.CharField(max_length=255),
+    '__unicode__': lambda self: eval("self."+"name"),
+})
+admin.site.register(obj)
+
 sync_meta_models()
+
 
 # TODO: verificare se esiste una sorta di elenco di models.Fields
 #       in modo che nell'interfaccia di definizione del tipo
