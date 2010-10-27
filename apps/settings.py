a403a680c7ae19a8db5ca6657fa5a9d4b51db963
ferama <marco.ferragina@oktago.com>
2010-10-27 11:34:12 +0200

    test middleware

--- apps/settings.py
+++ apps/settings.py
@@ -84,6 +84,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'meta.middleware.MetaObjectsMiddleware',
 )
 
 ROOT_URLCONF = 'urls'
