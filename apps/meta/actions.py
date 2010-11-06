7ca3afad2ae61a4a6840bd833b6c27cb21a225c1
ferama <marco.ferragina@oktago.com>
2010-11-06 17:44:39 +0100

    added vim swp files to git ignore and named urls

--- apps/meta/actions.py
+++ apps/meta/actions.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from django.forms.models import modelform_factory
+from django.core.urlresolvers import reverse
 
 """
 Action class for meta model.
@@ -16,12 +17,12 @@ class Actions:
         """
         return HttpResponse("index: %s" % self.model._meta)
 
-    def single(self, request, objId):
+    def details(self, request, objId):
         """
         Render a single object
         """
         obj = self.model.objects.get(pk=objId)
-        return HttpResponse("single")
+        return HttpResponse("details")
 
     def add(self, request):
         ModelForm = modelform_factory(self.model)
@@ -29,7 +30,7 @@ class Actions:
             form = ModelForm(request.POST)
             if form.is_valid():
                 form.save()
-                #redirect("index")
+                return redirect(reverse(self.__url_name("index")))
         else:
             form = ModelForm()
         context = {
@@ -44,14 +45,17 @@ class Actions:
     def change(self, request, objId):
         return HttpResponse("change")
 
+    def __url_name(self, view_name):
+        return '%s_%s' % (self.model.__name__, view_name)
+
     def get_urls(self):
         from django.conf.urls.defaults import patterns, url
         urlpatterns = patterns('',
-            url(r'^$', self.index, name='index'),
-            url(r'^add/$', self.add),
-            url(r'^(.+)/delete/$', self.delete),
-            url(r'^(.+)/change/$', self.change),
-            url(r'^(.+)/$', self.single),
+            url(r'^$', self.index, name = self.__url_name("index")),
+            url(r'^add/$', self.add, name = self.__url_name("add")),
+            url(r'^(.+)/delete/$', self.delete, name = self.__url_name("delete")),
+            url(r'^(.+)/change/$', self.change, name = self.__url_name("change")),
+            url(r'^(.+)/$', self.details, name = self.__url_name("details")),
         )
         return urlpatterns
     urls = property(get_urls)
