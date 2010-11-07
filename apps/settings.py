e5bd45c1d901a21e862481cb6c78a2755d9852a9
ferama <marco.ferragina@oktago.com>
2010-11-07 16:44:50 +0100

    it language

--- apps/settings.py
+++ apps/settings.py
@@ -38,13 +38,13 @@ TIME_ZONE = 'Europe/Rome'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'it-IT'
 
 SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
-USE_I18N = True
+USE_I18N = False 
 
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale
